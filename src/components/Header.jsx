f8019f802ff6cbe95188f4818b543b7895eec64a
Daniel <daniel@Daniels-MBP.attlocal.net>
2024-08-11 23:58:13 -0400

    did headerSection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import { Box } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
+  const [isopen, setOpen] = useState(false);
+
+  const HandleOpen = () => {
+    setOpen(true)
+  }
+  const HandleClose = () => {
+    setOpen(false)
+  }
   return (
-    <div>Header</div>
+    <>
+      <header className='h-20 flex items-center justify-between'>
+        <div className='flex items-center gap-1 ml-10'>
+          <Box className=" w-10 h-10"><StorefrontIcon style={{ fontSize: '40px' }} /></Box>
+          <p className='text-2xl'>Ebzy</p>
+        </div>
+        <nav className='list-none sm:hidden  flex w-[600px] justify-between text-xl text-gray-400 capitalize mr-10  '>
+          <li>All Products</li>
+          <li>About Us</li>
+          <li>FAQ</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </nav>
+        {
+          !isopen ? <Box onClick={HandleOpen} className="hidden cursor-pointer sm:block "><MenuIcon className=' mr-5 ' /></Box>
+            : <Box onClick={HandleClose} className="hidden cursor-pointer sm:block " ><CloseIcon className=' mr-5 ' /></Box>
+        }
+        <nav style={{top: isopen ? "75px" : "-500px", transitionDelay: ".5s"}} className='list-none flex flex-col px-5 py-5 justify-between text-xl text-gray-400 capitalize mr-10 h-80 w-full sm:flex md:hidden absolute transition-all'>
+          <li>All Products</li>
+          <hr className='border-2 border-gray-300 border-t-0' />
+          <li>About Us</li>
+          <hr className='border-2 border-gray-300 border-t-0' />
+          <li>FAQ</li>
+          <hr className='border-2 border-gray-300 border-t-0' />
+          <li>Blog</li>
+          <hr className='border-2 border-gray-300 border-t-0' />
+          <li>Contact</li>
+        </nav>
+
+      </header>
+
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ export default {
       '2xl': {'max': '1535px'}, // => @media (max-width: 1535px) { ... }
       'xl': {'max': '1279px'}, // => @media (max-width: 1279px) { ... }
       'lg': {'max': '1023px'}, // => @media (max-width: 1023px) { ... }
-      'md': {'max': '767px'}, // => @media (max-width: 767px) { ... }
-      'sm': {'max': '639px'}, // => @media (max-width: 639px) { ... }
+      'md': {'min': '768px'}, // => @media (max-width: 767px) { ... }
+      'sm': {'max': '768px'}, // => @media (max-width: 639px) { ... }
     },
 
   },
